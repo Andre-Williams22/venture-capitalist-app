cf8d8c6c10f43a12e3e315ad84177df95b98de9b
Andre Williams <awjrs22@gmail.com>
2020-03-19 17:02:56 -0700

    add flask-wtforms for the registration page

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
 
+from wtform_fields import *
+
+
 app = Flask(__name__)
 # to keep clientside sessions secure 
 app.secret_key = 'replace later'
@@ -7,7 +10,11 @@ app.secret_key = 'replace later'
 @app.route("/", methods=['GET', 'POST'])
 def index():
 
-    return render_template("index.html")
+    reg_form = RegistrationForm()
+    if reg_form.validate_on_submit():
+        return "great success"
+
+    return render_template("index.html", form=reg_form)
 
 
 if __name__ == '__main__':

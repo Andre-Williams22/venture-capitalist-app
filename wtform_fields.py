e5f883e2dc7ed939e0c754a0105073eaf1348a4f
Andre Williams <awjrs22@gmail.com>
2020-03-19 20:14:24 -0700

    user form validation and duplicate username checker finished

--- wtform_fields.py
+++ wtform_fields.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, EqualTo, ValidationError
-
+from models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -10,3 +10,8 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('password_label', validators=[InputRequired(message="Pasword required"), Length(min=6, max=25, message="Password must be between 6 and 25 characters")])
     confirm_pswd = PasswordField('confirm_pswd_label', validators=[InputRequired(message="Password required"), EqualTo('password', message="Passwords must match")])
     submit_button = SubmitField('Create')
+
+    def validate_username(self, username):
+        user_object = User.query.filter_by(username=username.data).first() # the value the user puts into web page
+        if user_object: # if user_object already exists 
+            raise ValidationError("Username already exists. Please select a different username.")

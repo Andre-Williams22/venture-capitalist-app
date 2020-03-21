55ac56e8880b46bf4a17cacfecadfc33e3458e7f
Andre Williams <awjrs22@gmail.com>
2020-03-20 17:15:51 -0700

    implements heroku dependencies

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from time import localtime, strftime
 from flask import Flask, render_template, url_for, request, redirect, flash
 from flask_login import LoginManager, login_user, current_user, login_required, logout_user
-from flask_socketio import SocketIO, join_room, leave_room, send
+# from flask_socketio import SocketIO, join_room, leave_room, send
 from sklearn.externals import joblib
 import pandas as pd 
 import numpy as np 
@@ -14,18 +14,18 @@ from models import *
 
 app = Flask(__name__)
 # to keep clientside sessions secure 
-app.secret_key = 'replace later'
+app.secret_key =os.environ.get('SECRET')
 
 # Configure database
-app.config['SQLALCHEMY_DATABASE_URI']='postgres://nutgejunpisnuf:00c0a8255cc7ecbc5ad01197d0933906b367815398be4ce7811ffc46562f79bf@ec2-35-172-85-250.compute-1.amazonaws.com:5432/d9q7ih6h1mkskb'
+app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URL')
 # To Access Database in terminal:
 # psql (copy postgreslink....)
 
 
 db = SQLAlchemy(app)
 
-#Initialize flask socketio
-socketio = SocketIO(app)
+# #Initialize flask socketio
+# socketio = SocketIO(app)
 # Predefined rooms for chat
 ROOMS = ["lounge", "news", "Venture Capitalist", "Angel Investor", "Startup Members"]
 
@@ -72,19 +72,19 @@ def login():
         user_object = User.query.filter_by(username=login_form.username.data).first()
         login_user(user_object)
         
-        return redirect(url_for('chat')) # calls the chat function 
+        return redirect(url_for('home')) # calls the chat function 
 
     return render_template('login.html', form=login_form)
 
-@app.route("/chat", methods=['GET', 'POST'])
+@app.route("/home", methods=['GET', 'POST'])
 @login_required
-def chat():
+def home():
     '''Makes sure that user is logged in before accessing product '''
     if not current_user.is_authenticated:
         flash('Please login', 'danger')
         return redirect(url_for('login'))
 
-    return render_template("home.html", username=current_user.username, rooms=ROOMS)
+    return render_template("home.html", username=current_user.username)
 
 @app.route('/logout', methods=['GET'])
 def logout():
@@ -125,4 +125,4 @@ def predict():
 
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    app.run(debug=True)
